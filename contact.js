73ae8ddd2e91eec68454fb239e4562ab69c55afc
Monika Blakely <140799824+monikablakely@users.noreply.github.com>
2026-03-23 21:45:14 -0400

    Delete contact.js

--- contact.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById('contactForm');
-form.addEventListener('submit', function(event) {
-  if (!form.checkValidity()) {
-    event.preventDefault();
-    alert('Please fill out all fields correctly before submitting.');
-  }
-});
